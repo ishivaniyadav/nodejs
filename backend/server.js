db6a396c0379a6b800cb22e22de555ea0994ee52
Shivani Yadav <ishivaniyadav7@gmail.com>
2025-08-13 10:52:35 +0530

    subdmodlue

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, Date.now() + '-' + file.originalname),
+  filename: (req, file, cb) => cb(null, Date.now() + '-'  + file.originalname),
 });
 
 const upload = multer({ storage });
